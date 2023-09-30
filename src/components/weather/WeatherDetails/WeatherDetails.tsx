e9eca8f8d72803223516b9da6ca1b029fbdb005d
Peter Bonev <pebonew@gmail.com>
2023-09-30 15:48:10 +0300

    Update the styles to fit different devices

--- src/components/weather/WeatherDetails/WeatherDetails.tsx
+++ src/components/weather/WeatherDetails/WeatherDetails.tsx
@@ -7,17 +7,31 @@ import Loader from '../../common/Loader/Loader';
 import WeatherDetailsItem from './WeatherDetailsItem/WeatherDetailsItem';
 import styles from './WeatherDetails.module.css';
 
+const ITEMS_PER_GROUP = 2;
+
 const WeatherDetails: FC = () => {
     const { id = '' } = useParams();
     const { byId } = useContext(ForecastContext);
     const data = byId[id];
+    const weatherGroupsCount = data ? Math.ceil(data.length / ITEMS_PER_GROUP) : 0;
     const date = moment(id);
 
     const renderDetails = useCallback((item: ForecastData) => (
         <WeatherDetailsItem key={`${item.dt}`} {...item} />
     ), []);
 
-    return id && data ? (
+    const renderWeatherGroup = useCallback((_: unknown, index: number ) => {
+        const startIndex = index * ITEMS_PER_GROUP;
+        const items = data.slice(startIndex, startIndex + ITEMS_PER_GROUP);
+
+        return (
+            <div key={index} className={styles['weather-details-group']}>
+                {items.map(renderDetails)}
+            </div>
+        );
+    }, [data, renderDetails]);
+
+    return id && weatherGroupsCount ? (
         <div className={styles['weather-details-container']}>
             <div className={styles['weather-details-heading']}>
                 {date.format(DAY_FORMAT)}
@@ -26,7 +40,7 @@ const WeatherDetails: FC = () => {
                 {date.format(DATE_FORMAT)}
             </div>
             <div className={styles['weather-details']}>
-                {data.map(renderDetails)}
+                {Array.from({ length: weatherGroupsCount }).map(renderWeatherGroup)}
             </div>
         </div>
     ) : <Loader />;
